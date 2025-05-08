b4871f0e0d68eed5c936d0404754a59fef361a95
tinkermanz <m.b.istiak@outlook.com>
2025-05-08 11:30:37 +0600

    /latest-blogs route created

--- backend-js/server.js
+++ backend-js/server.js
@@ -259,6 +259,18 @@ const verifyJWT = (req, res, next) => {
 	})
 }
 
+
+server.get('/latest-blogs', (req, res, next)=> {
+	let maxLimit = 5
+
+	Blog.find({draft: false}).populate('author', 'personal_info.profile_img personal_info.username personal_info.fullname -_id').sort({'publishedAt': -1}).select('blog_id title des banner activity tags publishedAt -_id').limit(maxLimit).then(blogs => {
+		return res.status(200).json({blogs})
+	}).catch(err => {
+		return res.status(500).json({error: err.message})
+	})
+} )
+
+
 server.post('/create-blog', verifyJWT, (req, res, next)=> {
 
 	let authorId = req.user;
